6bafef5d4d7da3432842d52e98b5babb7ac56fd8
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-08 13:33:15 -0500

    :wastebasket: clean code and unused dependencies

--- app/src/main/java/com/mss/asistenciaapp/ui/AddAsistenciaScreen.kt
+++ app/src/main/java/com/mss/asistenciaapp/ui/AddAsistenciaScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
