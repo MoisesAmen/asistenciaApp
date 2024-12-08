8731f946cf2ad0a17d399d28ce51499c1bcb2890
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-07 23:16:53 -0500

    :rocket: add ip to database

--- app/src/main/java/com/mss/asistenciaapp/data/network/ApiClient.kt
+++ app/src/main/java/com/mss/asistenciaapp/data/network/ApiClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://192.168.1.4:8080/api/"
+    private const val BASE_URL = "http://192.168.100.51:8080/api/"
 
     private val retrofit by lazy {
         Retrofit.Builder()
